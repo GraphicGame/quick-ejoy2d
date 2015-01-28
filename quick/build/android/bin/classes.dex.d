4215c1137da30b4f3a861bb32faa5ae402b1ec7a
Graphics <codinglikecrazy@gmail.com>
2015-01-28 23:33:08 +0800

    pack.

--- quick/build/android/bin/classes.dex.d
+++ quick/build/android/bin/classes.dex.d
@@ -1,13 +1,13 @@
-D:\Lab\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes.dex : \
-D:\Lab\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\gl2jni\GL2JNIActivity.class \
-D:\Lab\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\gl2jni\GL2JNILib.class \
-D:\Lab\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\gl2jni\GL2JNIView$1.class \
-D:\Lab\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\gl2jni\GL2JNIView$ConfigChooser.class \
-D:\Lab\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\gl2jni\GL2JNIView$ContextFactory.class \
-D:\Lab\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\gl2jni\GL2JNIView$Renderer.class \
-D:\Lab\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\gl2jni\GL2JNIView.class \
-D:\Lab\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\rainblood\BuildConfig.class \
-D:\Lab\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\rainblood\R$attr.class \
-D:\Lab\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\rainblood\R$drawable.class \
-D:\Lab\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\rainblood\R$string.class \
-D:\Lab\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\rainblood\R.class \
+D:\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes.dex : \
+D:\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\gl2jni\GL2JNIActivity.class \
+D:\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\gl2jni\GL2JNILib.class \
+D:\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\gl2jni\GL2JNIView$1.class \
+D:\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\gl2jni\GL2JNIView$ConfigChooser.class \
+D:\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\gl2jni\GL2JNIView$ContextFactory.class \
+D:\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\gl2jni\GL2JNIView$Renderer.class \
+D:\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\gl2jni\GL2JNIView.class \
+D:\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\rainblood\BuildConfig.class \
+D:\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\rainblood\R$attr.class \
+D:\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\rainblood\R$drawable.class \
+D:\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\rainblood\R$string.class \
+D:\engine_ejoy2d_labs\quick-ejoy2d.git\quick\build\android\bin\classes\com\engine\rainblood\R.class \
