6b63c23b80de96f632b3adc0464b499b0e3e3f1e
Graphics <codinglikecrazy@gmail.com>
2015-01-29 19:10:52 +0800

    先提交吧。接下来，我可能会完全的重新设计quick-ejoy2d引擎的用户接口。

--- quick/ui/UIDisplayObjectContainer.h
+++ quick/ui/UIDisplayObjectContainer.h
@@ -3,6 +3,7 @@
 
 #include "UIInteractiveObject.h"
 #include "UINameSpace.h"
+#include <string>
 
 NS_UI_BEGIN
 
@@ -10,6 +11,25 @@ class UIDisplayObjectContainer : public UIInteractiveObject {
 public:
 	UIDisplayObjectContainer();
 	~UIDisplayObjectContainer();
+public:
+	bool getTouchChildren() const;
+	void setTouchChildren(bool value);
+
+	int getChildrenCount() const;
+
+	const UIDisplayObject * addChild(UIDisplayObject *child);
+	const UIDisplayObject * addChildAt(UIDisplayObject *child, int index);
+	bool contains(UIDisplayObject *child) const;
+	UIDisplayObject * getChildAt(int index);
+	UIDisplayObject * getChildByName(std::string name);
+	int getChildIndex(UIDisplayObject *child);
+
+protected:
+	void addNumChildren(int count);
+	void setNumChildren(int count);
+protected:
+	bool _touchChildren;
+	int _numChildren;
 };
 
 NS_UI_END
